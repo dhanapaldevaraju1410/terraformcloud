8da89bdeeecfa57e9719647029c3069d4879be87
dhanapaldevaraju1410 <dhanapal.devaraju@atyeti.com>
2025-03-03 14:05:01 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,19 +1,1 @@
-resource "google_compute_instance" "vm_instance" {
-  name         = "my-instance"
-  machine_type = "e2-micro"
-  zone         = "us-central1-a"
 
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-    }
-  }
-
-  network_interface {
-    network = "default"
-
-    access_config {
-      // Ephemeral IP
-    }
-  }
-}
