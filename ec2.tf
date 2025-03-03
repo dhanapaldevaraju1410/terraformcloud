91820f24352edabe14577b420004cb45e7378dd8
dhanapaldevaraju1410 <dhanapal.devaraju@atyeti.com>
2025-03-03 11:51:03 +0530

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,15 @@
+resource "google_compute_instance" "default" {
+  name         = "my-instance"
+  machine_type = "n1-standard-1"
+  zone         = "us-central1-a"
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "value"
+      }
+    }
+  }
