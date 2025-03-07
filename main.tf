63ac8d70187732fb000e2a446c7bc163ef9bd8a7
dhanapaldevaraju1410 <dhanapal.devaraju@atyeti.com>
2025-03-07 16:16:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,22 @@
+provider "aws" {
+  region     = "us-west-2"
+  access_key = "AKIAZI2LIKSNMQ2PIPFT"
+  secret_key = "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"
+}
+
+provider "google" {
+  project     = "natural-region-452705-m6"
+  region      = "us-central1"
+  credentials = "${path.module}/credentials.json"
+}
+
 resource "google_project_service" "storage_transfer" {
   project = "natural-region-452705-m6"
   service = "storagetransfer.googleapis.com"
 }
 
 resource "google_pubsub_topic" "transfer_notifications" {
-  name    = "pubsub1412"
+  name    = "pubsub1414"
   project = "natural-region-452705-m6"
 }
 
@@ -23,9 +35,15 @@ data "google_storage_transfer_project_service_account" "default" {
 
 resource "google_storage_bucket" "gcs_bucket" {
   count         = 2
-  name          = "gdhan${count.index}"
+  name          = "gdhanap${count.index}"
   location      = "US"
   storage_class = "STANDARD"
+}
+
+
+resource "aws_s3_bucket" "s3_bucket" {
+  count  = 2
+  bucket = "s3dha-${count.index}"
 }
 
 resource "google_storage_bucket_iam_member" "gcs_bucket" {
@@ -44,17 +62,6 @@ resource "google_storage_bucket_iam_member" "storage_transfer_service_account" {
   depends_on = [google_storage_bucket.gcs_bucket]
 }
 
-resource "aws_s3_bucket" "s3_bucket" {
-  count  = 2
-  bucket = "s3dhan-${count.index}"
-
-  lifecycle {
-    ignore_changes = [
-      bucket
-    ]
-  }
-}
-
 resource "google_storage_transfer_job" "s3_to_gcs" {
   count       = 2
   description = "Transfer data from S3 to GCS"
@@ -64,8 +71,8 @@ resource "google_storage_transfer_job" "s3_to_gcs" {
     aws_s3_data_source {
       bucket_name = aws_s3_bucket.s3_bucket[count.index].bucket
       aws_access_key {
-        access_key_id     = "AKIAZI2LIKSNABYWXWEH"
-        secret_access_key = "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"
+        access_key_id     = "AKIAZI2LIKSNMQ2PIPFT"
+        secret_access_key = "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"
       }
     }
 
@@ -101,4 +108,3 @@ resource "google_storage_transfer_job" "s3_to_gcs" {
     google_pubsub_topic.transfer_notifications
   ]
 }
-
