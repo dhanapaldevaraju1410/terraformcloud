944222366418199db08f60df532f343cc484060c
dhanapaldevaraju1410 <dhanapal.devaraju@atyeti.com>
2025-03-10 14:15:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,6 @@
 provider "aws" {
   region = "us-west-2"
-  
-  assume_role {
-    role_arn     = "arn:aws:iam::TARGET_ACCOUNT_ID:role/YOUR_ROLE_NAME"
-    session_name = "TerraformSession"  # Optional, for session naming
   }
-}
 
 provider "google" {
   project     = "natural-region-452705-m6"
