1f5698839d917c5588900f62678a094a6cdbf51b
dhanapaldevaraju1410 <dhanapal.devaraju@atyeti.com>
2025-03-13 12:17:21 +0530

    Update managed instance.tf

--- managed instance.tf
+++ managed instance.tf
@@ -1,8 +1,3 @@
-provider "google" {
-  project = var.project_id
-  region  = var.region
-}
-
 resource "google_compute_instance_template" "GCIT" {
   name         = "instance-template"
   machine_type = var.machine_type
