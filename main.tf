70169ce24b03095e8aed7112855824cd9f94e5a9
dhanapaldevaraju1410 <dhanapal.devaraju@atyeti.com>
2025-03-13 10:26:02 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,33 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+resource "google_compute_instance_template" "GCIT" {
+  name         = "instance-template"
+  machine_type = var.machine_type
+
+  disk {
+    source_image = var.source_image
+    auto_delete  = true
+    boot         = true
+  }
+
+  network_interface {
+    network = var.network
+    access_config {
+      // Ephemeral IP
+    }
+  }
+}
+
+resource "google_compute_instance_group_manager" "MIG" {
+  name               = "managed-instance-group"
+  base_instance_name = "instance"
+  zone               = var.zone
+  target_size        = var.instance_count
+
+  version {
+    instance_template = google_compute_instance_template.GCIT.self_link
+  }
+}
